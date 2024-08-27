016323af69ecb1eefe1fc7b799237394ea453f19
Benedictus Christo Geroda Cinun <benedictuschristo4@gmail.com>
2024-08-27 10:21:12 +0700

    Add files via upload

--- kinematics.m
+++ kinematics.m
@@ -1,16 +1,4 @@
-function [s] = kinematics(q, dq)
-global p1 p2 p3 p4 p5 p6
-global p1_s p2_s p3_s p4_s p5_s p6_s
-global l1 l2 l3 l4 l5 l6
-global s1 s2 s3 s4 s5 s6
-global n1 n2 n3 n4 n5 n6
-global R RT
-global qp1 qp2 qp3 qp4 qp5 qp6
-global dqp1 dqp2 dqp3 dqp4 dqp5 dqp6
-global n1_skew n2_skew n3_skew n4_skew n5_skew n6_skew
-global W_tilde
-global rp rb
-
+function [s] = kinematics(rp, rb, sigma, q)
 %% Parameters
 x = q(1);
 y = q(2);
@@ -19,25 +7,8 @@ phi = q(4);
 theta = q(5);
 psi = q(6);
 
-dx = dq(1);
-dy = dq(2);
-dz = dq(3);
-dphi = dq(4);
-dtheta = dq(5);
-dpsi = dq(6);
-
 t = [x; y; z];
-vt = [dx; dy; dz];
 
-sigma = deg2rad(15);
-
-qt = [phi; theta; psi];
-vqt = [dphi; dtheta; dpsi];
-
-%% Angular Velocity
-
-W = [1 0 -sin(theta); 0 cos(phi) cos(theta)*sin(phi); 0 -sin(phi) cos(theta)*cos(phi)]*[dphi; dtheta; dpsi];
-W_tilde = [0 -W(3) W(2); W(3) 0 -W(1); -W(2) W(1) 0];
 
 %% Frame {B}
 beta1 = deg2rad(120);
@@ -84,13 +55,6 @@ p4 = [-rp*sin(p4_angle); rp*cos(p4_angle); 0];
 p5 = [-rp*sin(p5_angle); rp*cos(p5_angle); 0];
 p6 = [-rp*sin(p6_angle); rp*cos(p6_angle); 0];
 
-%Skew Symmetric Matrix of P --> For Dynamics
-p1_s = [0 -p1(3) p1(2); p1(3) 0 -p1(1); -p1(2) p1(1) 0];
-p2_s = [0 -p2(3) p2(2); p2(3) 0 -p2(1); -p2(2) p2(1) 0];
-p3_s = [0 -p3(3) p3(2); p3(3) 0 -p3(1); -p3(2) p3(1) 0];
-p4_s = [0 -p4(3) p4(2); p4(3) 0 -p4(1); -p4(2) p4(1) 0];
-p5_s = [0 -p5(3) p5(2); p5(3) 0 -p5(1); -p5(2) p5(1) 0];
-p6_s = [0 -p6(3) p6(2); p6(3) 0 -p6(1); -p6(2) p6(1) 0];
 
 %% Platform Rotation Matrix
 Rz = [cos(psi) -sin(psi) 0; sin(psi) cos(psi) 0; 0 0 1]; %yaw
@@ -98,7 +62,6 @@ Ry = [cos(theta) 0 sin(theta); 0 1 0; -sin(theta) 0 cos(theta)]; %pitch
 Rx = [1 0 0; 0 cos(phi) -sin(phi); 0 sin(phi) cos(phi)]; %roll
 
 R = Rz*Ry*Rx; %3D Rotation Matrix
-RT = transpose(R);
 
 %% Upper Gimbal Point
 qp1 = t + (R*p1);
@@ -107,14 +70,6 @@ qp3 = t + (R*p3);
 qp4 = t + (R*p4);
 qp5 = t + (R*p5);
 qp6 = t + (R*p6);
-
-%% Upper gimbal point velocity
-dqp1 = [eye(3) R*transpose(p1_s)*RT]*dq;
-dqp2 = [eye(3) R*transpose(p2_s)*RT]*dq;
-dqp3 = [eye(3) R*transpose(p3_s)*RT]*dq;
-dqp4 = [eye(3) R*transpose(p4_s)*RT]*dq;
-dqp5 = [eye(3) R*transpose(p5_s)*RT]*dq;
-dqp6 = [eye(3) R*transpose(p6_s)*RT]*dq;
 
 %% li vector value
 l1 = t + (R*p1) - b1; %vektor kaki ke-1
@@ -133,30 +88,6 @@ s5 = sqrt(transpose(l5)*l5); %panjang kaki ke-5
 s6 = sqrt(transpose(l6)*l6); %panjang kaki ke-6
 
 s = [s1; s2; s3; s4; s5; s6];
-
-%% Unit Vector
-n1 = l1/s1;
-n2 = l2/s2;
-n3 = l3/s3;
-n4 = l4/s4;
-n5 = l5/s5;
-n6 = l6/s6;
-
-%% Skew Symmetric Matrix of n
-n1_skew = [0 -n1(3) n1(2); n1(3) 0 -n1(1); -n1(2) n1(1) 0];
-n2_skew = [0 -n2(3) n2(2); n2(3) 0 -n2(1); -n2(2) n2(1) 0];
-n3_skew = [0 -n3(3) n3(2); n3(3) 0 -n3(1); -n3(2) n3(1) 0];
-n4_skew = [0 -n4(3) n4(2); n4(3) 0 -n4(1); -n4(2) n4(1) 0];
-n5_skew = [0 -n5(3) n5(2); n5(3) 0 -n5(1); -n5(2) n5(1) 0];
-n6_skew = [0 -n6(3) n6(2); n6(3) 0 -n6(1); -n6(2) n6(1) 0];
-
-%% Legs Velocity Vector
-dl1 = transpose(n1)*dqp1;
-dl2 = transpose(n2)*dqp1;
-dl3 = transpose(n3)*dqp1;
-dl4 = transpose(n4)*dqp1;
-dl5 = transpose(n5)*dqp1;
-dl6 = transpose(n6)*dqp1;
 
 %% New platform coordinates
 x1 = [b1(1) b1(1)+l1(1)];
@@ -187,26 +118,26 @@ z_new = [b1(3)+l1(3) b2(3)+l2(3) b3(3)+l3(3) b4(3)+l4(3) b5(3)+l5(3) b6(3)+l6(3)
 %% Plotting Robot
 figure(1)
 clf;
-plot3(xb, yb, zb, '-o'); %plot base
+plot3(xb, yb, zb, '-o'); %plot the base
 
-title('Stewart Platform Simulation');  %judul plot
-xlabel('Sumbu X'); %label sumbu x
-ylabel('Sumbu Y'); %label sumbu y
-zlabel('Sumbu Z'); %label sumbu z
+title('Stewart Platform Simulation');  %the plot's title
+xlabel('X-Axis'); %x axis label
+ylabel('Y-Axis'); %y axis label
+zlabel('Z-Axis'); %z axis label
 
 axis([-rb-0.1 rb+0.1, -rb-0.1 rb+0.1, 0 0.6]);
 hold on;
-fill3(xb, yb, zb, 'c'); %memberi fill warna platform {B}
+fill3(xb, yb, zb, 'c'); %give color to the base
 
 %plot new coordinate
-plot3(x1,y1,z1, '-or', 'LineWidth', 3); %plot kaki ke-1
-plot3(x2,y2,z2, '-og', 'LineWidth', 3); %plot kaki ke-2
-plot3(x3,y3,z3, '-ob', 'LineWidth', 3); %plot kaki ke-3
-plot3(x4,y4,z4, '-oc', 'LineWidth', 3); %plot kaki ke-4
-plot3(x5,y5,z5, '-om', 'LineWidth', 3); %plot kaki ke-5
-plot3(x6,y6,z6, '-ok', 'LineWidth', 3); %plot kaki ke-6
+plot3(x1,y1,z1, '-or', 'LineWidth', 3); %1st leg's plot
+plot3(x2,y2,z2, '-og', 'LineWidth', 3); %2st leg's plot
+plot3(x3,y3,z3, '-ob', 'LineWidth', 3); %3st leg's plot
+plot3(x4,y4,z4, '-oc', 'LineWidth', 3); %4st leg's plot
+plot3(x5,y5,z5, '-om', 'LineWidth', 3); %5st leg's plot
+plot3(x6,y6,z6, '-ok', 'LineWidth', 3); %6st leg's plot
 
-plot3(x_new, y_new, z_new, '-o'); %plot platform
+plot3(x_new, y_new, z_new, '-o'); %plotting the platform
 
-fill3(x_new, y_new, z_new, 'm'); %memberi fill warna platform {P}
+fill3(x_new, y_new, z_new, 'm'); %give the platform color
 grid on;
---
+++ kinesim.m
@@ -0,0 +1,111 @@
+clc
+clear variables
+close all
+
+syms x(t) y(t) z(t) phi(t) theta(t) psi(t)
+
+dt = 0.01; %step
+t_max = 5; %max time
+t = 0:dt:t_max; %iteration
+
+rb = 0.2; %radius frame {B} in meter
+rp = 0.16; %radius frame {P} in meter
+sigma = deg2rad(15);
+
+%desired position
+x = 0;
+y = 0;
+z = 0.37;
+
+%desired orientation
+phi = deg2rad(5*sin(0.02*t*100)); %pitch
+theta = deg2rad(5*cos(0.02*t*100)); %roll
+psi = 0; %yaw
+
+
+
+s1 = [];
+s2 = []; 
+s3 = []; 
+s4 = []; 
+s5 = []; 
+
+%make a video of the simulation
+v = VideoWriter('KineSim.mp4','MPEG-4');
+
+open(v);
+for i = 1:length(t)
+    %if the position or the orientation changes , use the letter and add i
+                                %(example: x(i))
+    %if the position or the orientation doesn't change, just use the letter
+                                %(example: x)
+    q = [x; y; z; phi(i); theta(i); psi]; 
+    
+    [s] = kinematics(rp, rb, sigma, q);
+    
+    s1(i) = s(1);
+    s2(i) = s(2);
+    s3(i) = s(3);
+    s4(i) = s(4);
+    s5(i) = s(5);
+    s6(i) = s(6);
+    
+    frame = getframe(gcf);
+    writeVideo(v, frame);
+end
+close(v);
+
+%% Plotting Legs length
+figure(2)
+plot(t, s1, '.','Color',[0 0 0]);
+title("1st Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
+
+figure(3)
+plot(t, s2, '.','Color',[0 0 0]);
+title("2nd Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
+
+figure(4)
+plot(t, s3, '.','Color',[0 0 0]);
+title("3rd Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
+
+figure(5)
+plot(t, s4, '.','Color',[0 0 0]);
+title("4th Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
+
+figure(6)
+plot(t, s5, '.','Color',[0 0 0]);
+title("5th Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
+
+figure(7)
+plot(t, s6, '.','Color',[0 0 0]);
+title("6th Leg's Length");
+xlabel('Time (t)');
+ylabel('Length (m)');
+axis([0 t_max, 0.3 0.55]);
+grid on
+hold on
